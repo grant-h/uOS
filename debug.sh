60d57448672fbb1f3095803a3dccbc144b16b608
Grant Hernandez <grant.h.hernandez@gmail.com>
2017-09-04 14:43:00 -0700

    Update debug script and ensure that qemu is killed

--- debug.sh
+++ debug.sh
@@ -2,16 +2,20 @@
 QEMUPID="/tmp/uosqemu.pid"
 TARGET="boot/kernel.bin"
 
+exithook() {
+  # kill qemu once GDB exits
+  kill `cat $QEMUPID` > /dev/null 2>&1
+}
+
 # all commands now relative to script location
 cd $(dirname $0)
 
 # start qemu in the background with the CPU halted and waiting for GDB on TCP 1234 
-qemu-kvm $@ -daemonize -pidfile $QEMUPID -s -S -kernel $TARGET 
+qemu-system-i386 $@ -daemonize -pidfile $QEMUPID -s -S -kernel $TARGET 
+trap exithook EXIT
 
 # run GDB and attach to the qemu instance
-gdb $TARGET -ex 'target remote 127.0.0.1:1234'
+gdb $TARGET -nx -ex 'target remote 127.0.0.1:1234'
 
-# kill qemu once GDB exits
-kill `cat $QEMUPID` > /dev/null 2>&1
 
 exit 0
